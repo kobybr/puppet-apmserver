5c4054d1c7a37bc5be1f203ffc7dcdd21b0f12a6
Bradley Koby <kobyb@miamioh.edu>
2019-02-12 09:15:49 -0500

    Add version fact

---
+++ lib/facter/apm-server.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+require 'facter'
+
+Facter.add(:pkg_versions) do
+  confine osfamily: 'RedHat'
+
+  setcode do
+    pkg_versions = {}
+    pkg_versions[:apmserver] = Facter::Util::Resolution.exec('rpm -q --queryformat \'%{VERSION}-%{RELEASE}\n\' apm-server')
+    pkg_versions unless pkg_versions.empty?
+  end
+end
+
+Facter.add(:pkg_versions) do
+  confine osfamily: 'Debian'
+
+  setcode do
+    pkg_versions = {}
+    pkg_versions[:apmserver] = Facter::Util::Resolution.exec('/usr/bin/dpkg-query --showformat=\'${Version}\n\' --show apm-server')
+    pkg_versions unless pkg_versions.empty?
+  end
+end
